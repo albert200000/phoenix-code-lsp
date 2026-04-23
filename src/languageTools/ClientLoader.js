ad5a87e90ffbfc3b5ebdfa98e1c67ca59df64a72
albert200000 <albert200000@gmail.com>
2026-04-23 16:46:28 +0300

    first commit

---
+++ src/languageTools/ClientLoader.js
@@ -0,0 +1,139 @@
+/*
+ * Copyright (c) 2019 - present Adobe. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+/*eslint no-console: 0*/
+/*eslint max-len: ["error", { "code": 200 }]*/
+define(function (require, exports, module) {
+    "use strict";
+
+    var ToolingInfo = JSON.parse(require("text!languageTools/ToolingInfo.json")),
+        FileUtils = require("file/FileUtils"),
+        EventDispatcher = require("utils/EventDispatcher"),
+        BracketsToNodeInterface = require("languageTools/BracketsToNodeInterface").BracketsToNodeInterface,
+        NodeConnector = brackets.getModule("NodeConnector");
+
+    EventDispatcher.makeEventDispatcher(exports);
+    //Register paths required for Language Client and also register default brackets capabilities.
+    var _bracketsPath = FileUtils.getNativeBracketsDirectoryPath();
+    // The native directory path ends with either "test" or "src".
+    _bracketsPath = _bracketsPath.replace(/\/test$/, "/src"); // convert from "test" to "src"
+
+    var _modulePath = FileUtils.getNativeModuleDirectoryPath(module),
+        clientInfoNodeConnector = NodeConnector.createNodeConnector(
+            "lsp-client-info",
+            {}
+        ),
+        clientInfoLoadedPromise = null,
+        //Clients that have to be loaded once the LanguageClient info is successfully loaded on the
+        //node side.
+        pendingClientsToBeLoaded = [];
+
+    function syncPrefsWithDomain(languageToolsPrefs) {
+        if (clientInfoNodeConnector) {
+            clientInfoNodeConnector.execPeer("syncPreferences", languageToolsPrefs);
+        }
+    }
+
+    function _createNodeConnector(domainName) {
+        return NodeConnector.createNodeConnector(domainName, {});
+    }
+
+    function createNodeInterfaceForNodeConnector(languageClientNodeConnector) {
+        var nodeInterface = new BracketsToNodeInterface(languageClientNodeConnector);
+
+        return nodeInterface;
+    }
+
+    function _clientLoader(clientName, clientPromise) {
+        var languageClientInterface = createNodeInterfaceForNodeConnector(_createNodeConnector(clientName));
+
+        clientPromise.resolve({
+            name: clientName,
+            interface: languageClientInterface
+        });
+    }
+
+    function initiateLanguageClient(clientName) {
+        var result = $.Deferred();
+
+        initNodeClient(clientName);
+
+        //Only load clients after the LanguageClient Info has been initialized
+        if (!clientInfoLoadedPromise || clientInfoLoadedPromise.state() === "pending") {
+            var pendingClient = {
+                load: _clientLoader.bind(null, clientName, result)
+            };
+            pendingClientsToBeLoaded.push(pendingClient);
+        } else {
+            _clientLoader(clientName, result);
+        }
+
+        return result;
+    }
+
+    /**
+     * This function passes Brackets's native directory path as well as the tooling commands
+     * required by the LanguageClient node module. This information is then maintained in memory
+     * in the node process server for succesfully loading and functioning of all language clients
+     * since it is a direct dependency.
+     */
+    function sendLanguageClientInfo() {
+        //Init node with Information required by Language Client
+        clientInfoLoadedPromise = clientInfoNodeConnector.execPeer("initializePreferences", {
+            bracketsSourcePath: _bracketsPath,
+            toolingInfo: ToolingInfo
+        });
+
+        function logInitializationError() {
+            console.error("Failed to Initialize LanguageClient Module Information.");
+        }
+
+        //Attach success and failure function for the clientInfoLoadedPromise
+        clientInfoLoadedPromise.then(function (success) {
+            if (!success) {
+                logInitializationError();
+                return;
+            }
+
+            if (Array.isArray(pendingClientsToBeLoaded)) {
+                pendingClientsToBeLoaded.forEach(function (pendingClient) {
+                    pendingClient.load();
+                });
+            } else {
+                exports.trigger("languageClientModuleInitialized");
+            }
+            pendingClientsToBeLoaded = null;
+        }, function () {
+            logInitializationError();
+        });
+    }
+
+    function initNodeClient(clientName) {
+        clientInfoNodeConnector.execPeer("initClient", { clientName, serverOptions: {} });
+    }
+
+    sendLanguageClientInfo();
+
+    exports.initiateLanguageClient = initiateLanguageClient;
+    exports.syncPrefsWithDomain = syncPrefsWithDomain;
+});
